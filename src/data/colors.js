f3bab5f78c1812d7c6f281de5b1c28ae0fbff7c3
Rafal Pijet <stronglopez@wp.pl>
2019-08-23 12:09:23 +0200

    Prepare project

---
+++ src/data/colors.js
@@ -0,0 +1,21 @@
+const dataColors = [{
+    name: "violet",
+    color: "#a55eea"
+}, {
+    name: "blue",
+    color: "#0fb9b1"
+}, {
+    name: "red",
+    color: "#eb3b5a"
+}, {
+    name: "yellow",
+    color: "#fed330"
+}, {
+    name: "orange",
+    color: "#fa8231"
+}, {
+    name: "black",
+    color: "#000000"
+}];
+
+export default dataColors;
---
+++ src/data/tasks.js
@@ -0,0 +1,15 @@
+const tasks = [{
+    id: 1,
+    text: 'clean room'
+}, {
+    id: 2,
+    text: 'wash the dishes'
+}, {
+    id: 3,
+    text: 'feed my cat'
+}, {
+    id: 4,
+    text: "go shopping"
+}];
+
+export default tasks;
